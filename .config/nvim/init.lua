ae14dcd3fc9b32929748cc2ff539bfb3ef2dae38
Barrett Flowers <barrettjflowers@gmail.com>
2025-08-06 08:42:08 -0400

    simple nvim config w/ native lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,28 +1,25 @@
+-- .config/nvim/init.lua
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
 vim.o.termguicolors = true
 vim.o.wrap = false
-vim.o.tabstop = 4
+vim.o.tabstop = 2
 vim.o.swapfile = false
 vim.g.mapleader = " "
 vim.o.winborder = "rounded"
 vim.o.clipboard = "unnamedplus"
 
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
-
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
-
+-- plugin management
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	{ src = "https://github.com/github/copilot.vim" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/kevinhwang91/rnvimr" },
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -35,19 +32,31 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 vim.cmd("set completeopt+=noselect")
 
+-- init plugins
 require "mini.pick".setup()
+require"mason".setup()
 require "nvim-treesitter.configs".setup({
 	ensure_installed = { "svelte", "typescript", "javascript" },
 	highlight = { enable = true }
 })
-require "oil".setup()
+-- repalce netrw with rnvimr (ranger)
+vim.g.rnvimr_enable_ex = 1
+vim.g.loaded_netrw = 1
 
+-- key mappings
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
-vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+vim.keymap.set('n', '<leader>e', ":RnvimrToggle<CR>")
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-vim.lsp.enable({ "lua_ls", "biome", "tinymist", "emmetls" })
 
+-- LSP configuration
+local lspconfig = require "lspconfig"
+lspconfig.ts_ls.setup({})
+lspconfig.lua_ls.setup({})
+lspconfig.rust_analyzer.setup({})
+
+-- styling
 require "vague".setup({ transparent = true })
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
