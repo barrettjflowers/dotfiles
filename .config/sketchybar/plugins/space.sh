93e4ccf96acbc8fd52a43cb22556c42eb8c1270a
Barrett Flowers <barrettjflowers@gmail.com>
2025-11-18 15:07:44 -0500

    cleanup unused configs

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -2,10 +2,11 @@
 
 if [[ "$SELECTED" == "true" ]]; then
   sketchybar --set "$NAME" \
-    icon.color=0xFF000000 \
+	icon.color=0xFF000000 \
     background.color=0xFFFFFFFF
 else
   sketchybar --set "$NAME" \
-    icon.color=0xAAFFFFFF \
-    background.color=0x90222726
+   label.color=0xAAFFFFFF \
+   icon.color=0xAAFFFFFF \
+   background.drawing=off
 fi
